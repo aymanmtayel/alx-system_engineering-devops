3e0fd055388fe51436d1792bc5f0f333414b1149
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-20 15:26:08 +0200

    fix typos

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,13 @@ include stdlib
 file_line { 'Use Private key':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '	IdentifyFile ~/.ssh/school',
+  line    => '	IdentityFile ~/.ssh/school',
   replace => true,
 }
 
 file_line { 'Turn off password auth':
   ensure  => present,
-  path    => 'etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => '	PasswordAuthentication no',
   replace => true,
 }
