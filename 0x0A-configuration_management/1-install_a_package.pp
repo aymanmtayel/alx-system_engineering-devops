be2aabe8a91113ec5965defb1d92fc7e80938750
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-20 23:15:15 +0200

    Task01

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# instal flask from pip3 and ensure python3
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
+# install flask version 2.1.0 using pip
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
