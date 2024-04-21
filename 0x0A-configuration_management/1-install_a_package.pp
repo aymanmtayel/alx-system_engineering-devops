db15928422c2ffc10859d1bc4b7ecad85cd2c869
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-21 19:50:56 +0200

    task01 add werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-# install flask version 2.1.0 using pip
+# install flask version 2.1.0 using pip and dependant packages
+
+package { 'python3':
+  ensure => '3.8.10',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
