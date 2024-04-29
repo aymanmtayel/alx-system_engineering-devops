c2932e6eea10e683afefe1167f22c273f6bdc78d
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-29 23:21:50 +0300

    task02

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+#automate the task of creating a custom HTTP header response, but with Puppet.
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx
+}
+
+package {'nginx':
+  ensure => 'installed',
+}
+
+file_line {'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  line  => "    add_header X-Served-By \"$(hostname)\";",
+  match => 'http {',
+}
+
+exec {'start':
+  command => '/usr/sbin/service nginx start',
+}
